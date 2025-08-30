171d7e3f3427627fa89dec7c5b877ff62422b1cf
bloppo <parbanas@hotmail.com>
2025-08-30 16:32:22 -0700

    4th Commit - Cleaned up all lint warnings and errors

---
+++ src/assets/tododata.ts
@@ -0,0 +1,49 @@
+
+
+const todoData = [
+    {
+        recno: 1,
+        description: "Buy groceries",
+        position:"Greeter",
+        completed: false,
+        dueDate: "2025-06-10"
+    },
+    {
+        recno: 2,
+        description: "Dust bathroom",
+        position:"Flash Light Holder",
+        completed: false,
+        dueDate: "2025-06-12"
+    },
+    {
+        recno: 3,
+        description: "Scrub lights",
+        position:"Tool Whisperer",
+        completed: false,
+        dueDate: "2025-07-16"
+    },
+    {
+        recno: 4,
+        description: "Polish Fruit",
+        position:"Flash Light Holder",
+        completed: false,
+        dueDate: "2025-08-01"
+    },
+    {
+        recno: 5,
+        description: "Toss The Salad",
+        position:"Chair Cusion Tester",
+        completed: false,
+        dueDate: "2025-08-05"
+    },
+    {
+        recno: 6,
+        description: "Sweep street",
+        position:"Flash Light Holder",
+        completed: false,
+        dueDate: "2025-09-15"
+    },
+
+]
+
+export default todoData;
